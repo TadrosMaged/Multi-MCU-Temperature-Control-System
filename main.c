7631cdd418bbf8a39a99a95221307770173167e1
Tadros Maged <tadrosmaged30@gmail.com>
2024-08-11 18:04:27 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 11, 2024
+ *      Author: Tadros
+ */
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/eeprom.h>
+
+int main(void){
+
+	while(1){
+
+	}
+
+	return 0;
+}
