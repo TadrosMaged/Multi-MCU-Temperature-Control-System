fb639007485f177ea5db0623fa32942ab44036a8
Tadros Maged <tadrosmaged30@gmail.com>
2024-08-12 19:21:03 +0300

    Finished MCU1 except ADC and UART

--- main.c
+++ main.c
@@ -8,60 +8,188 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/eeprom.h>
+#include <avr/interrupt.h>
 
 #define normal_state 	 0
 #define emergency_state  1
 #define abnormal_state 	 2
-
+#define PWM_Fan_Pin		 PB3
+#define Fan_speed		 OCR0
 uint8_t current_state;
+uint8_t prev_state;
+
+float current_temp;
+float prev_temp;
+
+uint8_t init = 0;
+
 
 void normalState(void);
 void emergencyState(void);
 void abnormalState(void);
-void motorSpeed(int temp);
+
+void setState(uint8_t temp);
+
+void timer1_init(void);
+void watchdog_init(void);
 
 int main(void)
 {
+	//Enable interrupts
+	sei();
+
+	//PWM init
+	//Set pin as output
+	DDRB |= (1<<PWM_Fan_Pin);
+	//Set timer0 as fast PWM, OCR0 non-inverting and prescaler = 8
+	TCCR0 |= (1<<WGM00) | (1<<WGM01) | (1 << COM01) | (1 << CS01);
 
 	//EEPROM
-	//current_state = eeprom_read_byte((uint8_t*)0x00);
-	//eeprom_write_block((uint8_t*)0x00,normalState);
+	current_state=eeprom_read_byte((uint8_t*)0x00);
+	if(current_state == abnormal_state)
+	{
+		abnormalState();
+	}
 
-	//
+	//current_temp=read ADC
+
+	eeprom_write_byte((uint8_t*)0x00,normal_state);
 
 	while(1)
 	{
+		//current_temp=read_ADC
+		//send current_temp by uart
+		if( (current_temp != prev_temp) || (init==0) )
+		{
+			init=1;
+			if(current_temp <= 50)
+				current_state=normal_state;
+
+			else if(current_temp > 50)
+				current_state=emergency_state;
+
+			setState(current_temp);
+		}
+
+
 
 	}
 
 	return 0;
 }
 
-void normal_state(void)
+void setState(uint8_t temp)
 {
-	//From 20C to 40C fan increase gradually by mcu1
-	//mcu2 light yellow led
-	//
-	//Between 40C to 50C the fan is at max by mcu1
-	//mcu2 lights red and shut down motor if button pressed
+	if(current_state != prev_state)
+	{
+		switch (current_state)
+		{
+		case normal_state: normalState();
+		break;
+		case emergency_state: emergencyState();
+		break;
+		case abnormal_state: abnormalState();
+		break;
+		default: return;
+		}
+	}
+}
+void normalState(void)
+{
+	if(current_temp < 20)
+	{
+		//Stop fan
+		Fan_speed=0;
+	}
+	else if(current_temp >= 20 && current_temp < 40)
+	{
+		//Set fan speed corresponding to temp
+		Fan_speed=((current_temp-20)/20)*255;
+	}
+	else if(current_temp >= 40 && current_temp <= 50)
+	{
+		//Set fan speed to max
+		Fan_speed=255;
+	}
+	else if(current_temp > 50)
+	{
+		emergencyState();
+	}
 }
 
-void emergency_state(void)
+void emergencyState(void)
 {
-	//Above 50C
-	//Begin counting time.
-	//use timer with overflow to check temperature 14 times each 500ms
-	//MCU1 moves the fan with max speed
-	//MCU2 lightens red LED and runs buzzer.
-	//if activated for 7sec
-		//Write abnormal state in E2PROM
-		//MCU1 activates the watchdog timer with the smallest time to reset MCU1
-		//MCU2 follow the abnormal condition for 5 seconds
+	eeprom_write_byte((uint8_t*)0x00,emergency_state);
+
+	//Set fan speed to max
+	Fan_speed=255;
+
+	timer1_init();
+
+	watchdog_init();
+
 }
 
-void abnormal_state(void)
+void abnormalState(void)
 {
-	//set for 5 sec
-	//MCU1 moves the fan with max speed
-	//MCU2 lighten red LED, open servo with 90 degrees, stop the machine and run the buzzer
+	eeprom_write_byte((uint8_t*)0x00,abnormal_state);
+	Fan_speed=255;
 }
+
+
+void watchdog_init(void) {
+	//Disable interrupts
+    cli();
+
+    //Activate the watchdog timer with the smallest time to reset MCU1
+    WDTCR = (1 << WDE) | (1 << WDP0);
+
+    //Enable interrupts
+    sei();
+}
+
+void timer1_init(void)
+{
+		//Set timer to CTC mode and prescaler to 64
+		TCCR1B |= (1 << WGM12) | (1 << CS11) | (1 << CS10);
+
+		//Compare value= ( (8MHz * 500ms)/64 ) -1
+		OCR2 = 62499;
+
+		//Enable Timer2 compare interrupt
+		TIMSK |= (1 << OCIE2);
+}
+
+
+void button_init(void) {
+    //Set PB0 as input
+    DDRB &= ~(1 << PB0);
+
+     // The falling edge of INT0 generates an interrupt request
+    MCUCR = (1 << ISC01);
+
+     //Enable external interrupt on INT0
+    GICR = (1 << INT0);
+}
+
+ISR(TIMER1_COMPA_vect) {
+	//current_temp=ADC_read
+	static uint8_t count=0;
+	if(count == 14)
+	{
+		abnormalState();
+	}
+	else
+		count++;
+}
+
+ISR(INT0_vect) {
+
+    //current_temp = ADCread
+
+    if (current_temp >= 40 && current_temp <= 50) {
+        //send_shutdown_command();
+    }
+}
+
+
